43008681f50c5a4888e963d98541b0d527fd3798
deepthisanand2024 <deepthis.anand@gmail.com>
2024-06-03 15:20:17 -0400

    added a calculator module

---
+++ calculator/init.py
@@ -0,0 +1,9 @@
+"""Module providing a function printing sum and difference."""
+
+def add(a,b):
+    """Function printing sum of 2 numbers."""
+    return a + b
+
+def subtract(a,b):
+    """Function printing difference of 2 numbers."""
+    return a - b
